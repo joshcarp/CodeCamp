fce74f2613432e624d5961c2d50941499ec77219
Haohan Liu <the1nonlypanda@gmail.com>
2019-06-30 19:35:58 +1000

    Changes and additions to _*2str_() methods.
    Privacy levels changed. _func2str_() function created to streamline the outputting of input functions with all their *args and **kwargs.

--- unit-test/unit-test.py
+++ unit-test/unit-test.py
@@ -28,7 +28,7 @@ class UnitTest:
             return True
         return False
 
-    def args2str(self, args):
+    def _args2str_(self, args):
         """Return a string containing comma separated items from argument list."""
         # ensure that 'string' inputs are preserved with apostrophes
         str_args = []
@@ -40,7 +40,7 @@ class UnitTest:
         # return joined string of input arguments
         return ', '.join(x for x in str_args)
 
-    def kwargs2str(self, kwargs):
+    def _kwargs2str_(self, kwargs):
         """Return a string containing comma separated items from key argument list."""
         # ensure that 'string' key val inputs are preserved with apostrophes
         str_kwargs = []
@@ -52,6 +52,18 @@ class UnitTest:
         # return joined string of input key arguments
         return ', '.join(f"{k}={v}" for k, v in str_kwargs)
 
+    def _func2str_(self, args, kwargs):
+        """Return a string representation of the function tested."""
+        if args and kwargs:
+            f_pstr = f"{self.func}({self._args2str_(args)}, {self._kwargs2str_(kwargs)})"
+        elif args:
+            f_pstr = f"{self.func}({self._args2str_(args)})"
+        elif kwargs:
+            f_pstr = f"{self.func}({self._kwargs2str_(kwargs)})"
+        else:
+            f_pstr = f"{self.func}()"
+        return f_pstr
+
     def run(self, *args, **kwargs):
         """Run the function and print the function output given a set of arguments the function would naturally take.
 
@@ -62,20 +74,13 @@ class UnitTest:
         f_out = eval(f_str)
 
         # create function representation for print output
-        if args and kwargs:
-            f_pstr = f"{self.func}({self.args2str(args)}, {self.kwargs2str(kwargs)})"
-        elif args:
-            f_pstr = f"{self.func}({self.args2str(args)})"
-        elif kwargs:
-            f_pstr = f"{self.func}({self.kwargs2str(kwargs)})"
-        else:
-            f_pstr = f"{self.func}()"
+        f_pstr = self._func2str_(args, kwargs)
 
         # print output, if not explicitly disabled
         if not self._kw_handler_('noPrint', True, **kwargs):
             print(' [IN]', f_pstr)
 
-            # handle printing of types accuractely
+            # handle printing of string types accurately
             if isinstance(f_out, str):
                 print('[OUT]', f"'{f_out}'")
             else:
