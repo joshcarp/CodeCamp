a8f0f3eea6d6de4c10bdcf72e7c85aa8c9550ab1
Haohan Liu <the1nonlypanda@gmail.com>
2019-07-06 16:29:53 +1000

    Add python package installation script

---
+++ worksheet-template/install_python_packages.bat
@@ -0,0 +1,13 @@
+@echo off
+
+python -m pip install --upgrade pip
+
+pip install notebook
+
+pip install pytest
+
+pip install numpy
+
+echo ------------------------------------------------------------------------
+echo Python packages installed!
+PAUSE
